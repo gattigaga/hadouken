80031e1a961a59402a80451b44256e00c5bd7617
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2020-01-22 21:15:37 +0700

    Create CreateBoard component

---
+++ src/components/CreateBoard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 180px;
+  height: 96px;
+  background: #eee;
+  border-radius: 4px;
+  box-sizing: border-box;
+  padding: 12px;
+  display: flex;
+  cursor: pointer;
+`;
+
+const Label = styled.p`
+  font-family: "Roboto";
+  font-size: 14px;
+  color: #aaa;
+  margin: auto;
+`;
+
+const CreateBoard = ({ onPress }) => (
+  <Container onPress={onPress}>
+    <Label>Create New Board</Label>
+  </Container>
+);
+
+CreateBoard.propTypes = {
+  onPress: PropTypes.func
+};
+
+export default CreateBoard;

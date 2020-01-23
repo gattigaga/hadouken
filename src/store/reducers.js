5831ef40deabd778d4ecaaec4d2b735e959e01bc
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2020-01-23 23:55:39 +0700

    Create "lists" reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,4 +1,11 @@
-import { CREATE_BOARD, UPDATE_BOARD, DELETE_BOARD } from "./actions";
+import {
+  CREATE_BOARD,
+  UPDATE_BOARD,
+  DELETE_BOARD,
+  CREATE_LIST,
+  UPDATE_LIST,
+  DELETE_LIST
+} from "./actions";
 
 export const boards = (state = [], action) => {
   const { type, payload } = action;
@@ -17,6 +24,34 @@ export const boards = (state = [], action) => {
       });
 
     case DELETE_BOARD:
+      return state.filter(item => item.id !== payload);
+
+    default:
+      return state;
+  }
+};
+
+export const lists = (state = [], action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CREATE_LIST:
+      payload.index = state.filter(
+        item => item.boardId === payload.boardId
+      ).length;
+
+      return [...state, payload];
+
+    case UPDATE_LIST:
+      return state.map(item => {
+        if (item.id === payload.id) {
+          return { ...item, ...payload.data };
+        }
+
+        return item;
+      });
+
+    case DELETE_LIST:
       return state.filter(item => item.id !== payload);
 
     default:

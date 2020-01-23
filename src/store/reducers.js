751c45ead2688abff26db8eb53dfc9b3de7501e5
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2020-01-24 00:07:34 +0700

    Create "checks" reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -7,7 +7,10 @@ import {
   DELETE_LIST,
   CREATE_CARD,
   UPDATE_CARD,
-  DELETE_CARD
+  DELETE_CARD,
+  CREATE_CHECK,
+  UPDATE_CHECK,
+  DELETE_CHECK
 } from "./actions";
 
 export const boards = (state = [], action) => {
@@ -83,6 +86,34 @@ export const cards = (state = [], action) => {
       });
 
     case DELETE_CARD:
+      return state.filter(item => item.id !== payload);
+
+    default:
+      return state;
+  }
+};
+
+export const checks = (state = [], action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CREATE_CHECK:
+      payload.index = state.filter(
+        item => item.cardId === payload.cardId
+      ).length;
+
+      return [...state, payload];
+
+    case UPDATE_CHECK:
+      return state.map(item => {
+        if (item.id === payload.id) {
+          return { ...item, ...payload.data };
+        }
+
+        return item;
+      });
+
+    case DELETE_CHECK:
       return state.filter(item => item.id !== payload);
 
     default:

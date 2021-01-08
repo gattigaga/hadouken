9b18dc88bd97e44cb2926e807356531a4996c870
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2021-01-09 00:23:27 +0700

    Update CreateBoardButton component style

--- src/components/CreateBoardButton.js
+++ src/components/CreateBoardButton.js
@@ -5,14 +5,14 @@ import styled from "styled-components";
 const Button = styled.button`
   width: 180px;
   height: 96px;
-  background: #eee;
+  background: #ddd;
   border: 0px;
   border-radius: 4px;
   box-sizing: border-box;
   padding: 12px;
   font-family: "Roboto";
   font-size: 14px;
-  color: #aaa;
+  color: #999;
 `;
 
 const CreateBoardButton = ({ onClick }) => (
